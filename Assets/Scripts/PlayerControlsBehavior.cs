26bfd3acb93d5fc2c82d4c243966cdf443a5b50d
Anc0 <ak6688@student.uni-lj.si>
2019-01-05 22:34:11 +0100

    Terrain and Kurent movement implemented.

---
+++ Assets/Scripts/PlayerControlsBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlsBehavior : MonoBehaviour {
+
+	public float speed;
+	public float rotSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		 Rigidbody rb = GetComponent<Rigidbody>();
+         
+         if (Input.GetKey(KeyCode.W)) {
+             rb.MovePosition(transform.position + transform.forward * speed);
+		 }
+		 
+		 if (Input.GetKey(KeyCode.S)) {
+			rb.MovePosition(transform.position + transform.forward * - speed);
+		 }
+		 
+		 if (Input.GetKey(KeyCode.A)) {
+			Quaternion deltaRotation = Quaternion.Euler(0, - rotSpeed, 0);
+			rb.MoveRotation(rb.rotation * deltaRotation);
+		 }
+		 
+		 if (Input.GetKey(KeyCode.D)) {
+			Quaternion deltaRotation = Quaternion.Euler(0, rotSpeed, 0);
+			rb.MoveRotation(rb.rotation * deltaRotation);			
+		 }
+	}
+}
