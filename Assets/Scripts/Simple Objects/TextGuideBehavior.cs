21d14cde7a9a2dfe9330fd4346ad1c4f88a501e4
Anc0 <ak6688@student.uni-lj.si>
2019-01-11 10:22:13 +0100

    Started work on text guide.

---
+++ Assets/Scripts/Simple Objects/TextGuideBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextGuideBehavior : MonoBehaviour {
+
+	public Text guide;
+
+	private string[] LINES = {
+		"ASDFASDF"
+	};
+
+	// Use this for initialization
+	void Start () {
+		guide.text = LINES[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
