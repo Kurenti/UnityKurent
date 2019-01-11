84bde575728dfd24b8bc84ea065ebd8a5f9b40d0
Anc0 <ak6688@student.uni-lj.si>
2019-01-11 23:28:48 +0100

    Added pause and dead menu.

--- Assets/Scripts/Simple Objects/MenuBehavior.cs
+++ Assets/Scripts/Simple Objects/MenuBehavior.cs
@@ -10,11 +10,15 @@ public class MenuBehavior : MonoBehaviour {
 	public Button newGame;
 	public Button howToPlay;
 	public Button howToPlayBack;
+	public Button pauseResume;
 
 	public Text titleText;
 	public Text madeByText;
 	public Text howToPlayTitle;
 	public Text howToPlayContent;
+	public Text pauseTitle;
+	public Text deadTitle;
+	public Text deadContent;
 
 	public TextGuideBehavior tgb;
 
@@ -24,9 +28,16 @@ public class MenuBehavior : MonoBehaviour {
 		howToPlayTitle.gameObject.SetActive(false);
 		howToPlayContent.gameObject.SetActive(false);
 
+		pauseResume.gameObject.SetActive(false);
+		pauseTitle.gameObject.SetActive(false);
+
+		deadTitle.gameObject.SetActive(false);
+		deadContent.gameObject.SetActive(false);
+
 		newGame.onClick.AddListener(NewGameClick);
 		howToPlay.onClick.AddListener(HowToPlayClick);
 		howToPlayBack.onClick.AddListener(HowToPlayBackClick);
+		pauseResume.onClick.AddListener(Unpause);
 	}
 	
 	// Update is called once per frame
@@ -37,6 +48,9 @@ public class MenuBehavior : MonoBehaviour {
 	void NewGameClick() {
 		canvas.SetActive(false);
 		tgb.StartOpening();
+
+		deadTitle.gameObject.SetActive(false);
+		deadContent.gameObject.SetActive(false);
 	}
 
 	void HowToPlayClick() {
@@ -59,6 +73,57 @@ public class MenuBehavior : MonoBehaviour {
 		howToPlayBack.gameObject.SetActive(false);
 		howToPlayTitle.gameObject.SetActive(false);
 		howToPlayContent.gameObject.SetActive(false);
+	}
+
+	void Pause() {
+		canvas.SetActive(true);
+
+		newGame.gameObject.SetActive(false);
+		howToPlay.gameObject.SetActive(false);
+		titleText.gameObject.SetActive(false);
+		madeByText.gameObject.SetActive(false);
+
+		howToPlayBack.gameObject.SetActive(false);
+		howToPlayTitle.gameObject.SetActive(false);
+		howToPlayContent.gameObject.SetActive(false);
+
+		pauseTitle.gameObject.SetActive(true);
+		pauseResume.gameObject.SetActive(true);
+	}
+
+	void Unpause() {
+		canvas.SetActive(false);
+
+		newGame.gameObject.SetActive(true);
+		howToPlay.gameObject.SetActive(true);
+		titleText.gameObject.SetActive(true);
+		madeByText.gameObject.SetActive(true);
+
+		howToPlayBack.gameObject.SetActive(true);
+		howToPlayTitle.gameObject.SetActive(true);
+		howToPlayContent.gameObject.SetActive(true);
+
+		pauseTitle.gameObject.SetActive(false);
+		pauseResume.gameObject.SetActive(false);
+	}
+
+	void Died() {
+		canvas.SetActive(true);
+
+		newGame.gameObject.SetActive(true);
+		howToPlay.gameObject.SetActive(false);
+		titleText.gameObject.SetActive(false);
+		madeByText.gameObject.SetActive(false);
+
+		howToPlayBack.gameObject.SetActive(false);
+		howToPlayTitle.gameObject.SetActive(false);
+		howToPlayContent.gameObject.SetActive(false);
+
+		pauseTitle.gameObject.SetActive(false);
+		pauseResume.gameObject.SetActive(false);
+
+		deadTitle.gameObject.SetActive(true);
+		deadContent.gameObject.SetActive(true);
 	}
 
 }
